904484d4e014f982e747236e023782220aebdb90
Mason <mason@masons-macbook-air.local>
2015-11-09 17:11:36 -0500

    Initial Commit

---
+++ TimeTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeTable
+//
+//  Created by Mason on 2015-11-09.
+//  Copyright © 2015 Mason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
